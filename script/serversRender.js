562baefe1f8d45205d1efe6b84644f251cab150e
Alexander <kalinovsasha@yandex.ru>
2022-03-23 21:17:41 +0300

    feat: add fetch, update css

--- script/serversRender.js
+++ script/serversRender.js
@@ -26,13 +26,17 @@ const render = async function(){
         availableStatus=(servers[i].available)?"available":"notavailable";
         div.innerHTML += `
         <div class="main_serversAvaleble_server">
-        Name: ${servers[i].name}  Ip: ${servers[i].ip} Available: <div class="${availableStatus}">${servers[i].available}</div>
+        Name: ${servers[i].name}</br>  Ip: ${servers[i].ip}</br> Available: <div class="${availableStatus}">${servers[i].available}</div>
         </div>
         `;
-        document.body.append(div)
+        if(!servers[i].available){
+            fetch(`https://api.telegram.org/bot5199031552:AAHDifowU2CazGRqRHT1KgobJYKRw_QBSGc/sendMessage?chat_id=-653206323&text=${servers[1].name} ip=${servers[1].ip} is not available`)
+        }
     }
-    
-    
+    document.body.append(div)
+
 }
 
 render()
+
+//fetch(`https://api.telegram.org/bot5199031552:AAHDifowU2CazGRqRHT1KgobJYKRw_QBSGc/sendMessage?chat_id=-653206323&text=${servers[1].name}`)
